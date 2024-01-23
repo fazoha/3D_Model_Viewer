1ed978b29a2a3ea1e87279197220739beaad9edd
fazoha <atefzohaf@gmail.com>
2024-01-22 21:00:13 -0330

    test commit 1

--- js/viewer.js
+++ js/viewer.js
@@ -6,7 +6,7 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-
+//test test 123
 
 //Keep the 3D object on a global variable so we can access it later
 let object;
